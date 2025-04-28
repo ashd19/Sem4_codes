59e849661a15596d563c84b9317c573f65498d35
NaitikM11 <165246249+Naitik1104@users.noreply.github.com>
2025-04-28 19:06:52 +0530

    Update File_handling.py

--- python/File_handling.py
+++ python/File_handling.py
@@ -1,8 +1,29 @@
-file = "cities.txt"
-# 1 read the whole file contents 
+f = open("cities.txt","r") 
+print(f.read()) 
+f.close()
 
-# f = open(file, "r",encoding="utf-8")
-# print(f.read())
-# f.close()
+#Sort cities
+f = open("cities.txt","r") 
+k = f.readlines() 
+k.sort()
+f.close()
 
-    
+#Move the sorting in the different file
+f = open("sorted_cities.txt","a") 
+f.writelines(k)
+f.close()
+
+#Open that file
+f = open("sorted_cities.txt","r") print(f.read())
+
+#Reverse Sorting
+f = open("cities.txt","r") 
+k = f.readlines() 
+k.sort(reverse = "true") 
+f.close()
+
+f = open("desc_cities.txt","a") 
+f.writelines(k)
+f.close()
+f = open("desc_cities.txt","r") 
+print(f.read())
